513e1a6f1d93d7a0a287ce224d30a13d0c09cab4
Silver.Lee <68718384+godtechwak@users.noreply.github.com>
2023-01-04 19:37:11 +0900

    Create UpdateRow.sh

---
+++ UpdateRow.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+front_id=1
+back_id=1000
+max_id=11148508
+
+for (( ; ; ))
+do
+   MYSQL_PWD='{패스워드}' mysql -h{DB서버주소} -uadmin --comments -e "update test set seq=0 where id between ${front_id} and ${back_id}"
+
+   if [ ${max_id} -lt ${back_id} ]
+   then
+      break
+   fi
+   sleep 0.2
+
+   echo ${front_id}
+   echo ${back_id}
+   echo "==========="
+   front_id=$((front_id+1000))
+   back_id=$((back_id+1000))
+done
