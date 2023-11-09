3f25f38e7806d65d7db99ff55b1844b7c4f63756
Silver.Lee <68718384+godtechwak@users.noreply.github.com>
2023-11-09 11:50:30 +0900

    Update UpdateRow.sh

--- mysql/UpdateRow.sh
+++ mysql/UpdateRow.sh
@@ -2,27 +2,28 @@
 
 :<<'END'
 
-- 1,000건씩 배치 업데이트
+- 3,000건씩 배치 업데이트
 
 END
 
-front_id=1
-back_id=1000
-max_id=11148508
+front_id=1000
+back_id=4000
+max_id=10000
+add_count=3000
 
 for (( ; ; ))
 do
-   MYSQL_PWD='{패스워드}' mysql -h{DB서버주소} -uadmin --comments -e "update test set seq=0 where id between ${front_id} and ${back_id}"
+   MYSQL_PWD='{패스워드}' mysql -h{DB서버주소} -uadmin --comments -e "update test.test set seq=0 where id between ${front_id} and ${back_id}"
 
    if [ ${max_id} -lt ${back_id} ]
    then
       break
    fi
-   sleep 0.2
+   sleep 0.1
 
    echo ${front_id}
    echo ${back_id}
    echo "==========="
-   front_id=$((front_id+1000))
-   back_id=$((back_id+1000))
+   front_id=$((front_id+add_count))
+   back_id=$((back_id+add_count))
 done
